e0791d1cc1779eb165cd90d7b78b8af9479fdd6b
AsimCh-dev <asim@vrittechnologies.com>
2026-03-25 07:18:48 +0545

    added new data type folder

---
+++ DataTypes/string.py
@@ -0,0 +1,48 @@
+name = 'nepal'
+# print(name.capitalize())
+# print(name.lower())
+# print(name.upper())
+# print(name.swapcase())
+# print(name.title())
+
+
+# string format
+name = 'nepal'
+# print(len(name))
+print(id(name))
+# age = 20
+# print(f"my name is {name} and i am {age} years old")
+
+
+
+#variable[index]
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+
+
+
+#slicing in string 
+
+#variable[start:end:step]
+#nepal
+# print(name[2:5])
+# print(name[::])
+# print(name[::2])
+# print(name[::-1])
+
+name = name + ' '
+name = name + 'country'
+
+
+print(id(name))
+
+print(name)
+
+
+
+
+
+
+
